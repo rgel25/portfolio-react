19e8300ff2566d41d58388b3e0c564c51d40d1f2
Argel Miralles <argelmiralles25@gmail.com>
2022-08-18 15:33:47 +0800

    edited project arrangements

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,24 +1,6 @@
 const ProjectsData = [
   {
     id: 1,
-    name: "Sacred Symbols",
-    techStack: "MongoDB, Express, React, and NodeJS",
-    description:
-      "Sacred Symbols is an e-commerce web application built using the MERN Stack. It features modern UI/UX design, system architecture using MERN, and popular E-Commerce functionalities like shopping carts, account authentication and authorization, Admin dashboard, checkout flow integration with Paypal and Stripe",
-    url: "https://sacredsymbols.herokuapp.com/",
-    githubUrl: "https://github.com/rgel25/sacred-symbols",
-  },
-  {
-    id: 2,
-    name: "YelpCamp",
-    techStack: "MongoDB, Express, EJS and NodeJS",
-    description:
-      "YelpCamp is a simple review site for campgrounds. It is built upon the MVC architecture and uses RESTful API to manage data. The site features user authorization and authentication, campground, reviews and comments CRUD functionalities.",
-    url: "https://secure-castle-27004.herokuapp.com/",
-    githubUrl: "https://github.com/rgel25/yelpcamp",
-  },
-  {
-    id: 3,
     name: "Tracer",
     techStack: "MongoDB, Express, React, and NodeJS",
     description:
@@ -27,13 +9,31 @@ const ProjectsData = [
     githubUrl: "https://github.com/rgel25/tracer",
   },
   {
-    id: 4,
+    id: 2,
     name: "Talently",
     techStack: "React",
     description:
       "Talently is an online platform for job hunters and recruiters to connect with each other. It is a front-end application that mimics a database using the localStorage Web API. This project is mainly focused on React.",
     url: "https://talently-io.netlify.app/",
     githubUrl: "https://github.com/rgel25/talently",
+  },
+  {
+    id: 3,
+    name: "Sacred Symbols",
+    techStack: "MongoDB, Express, React, and NodeJS",
+    description:
+      "Sacred Symbols is an e-commerce web application built using the MERN Stack. It features modern UI/UX design, system architecture using MERN, and popular E-Commerce functionalities like shopping carts, account authentication and authorization, Admin dashboard, checkout flow integration with Paypal and Stripe",
+    url: "https://sacredsymbols.herokuapp.com/",
+    githubUrl: "https://github.com/rgel25/sacred-symbols",
+  },
+  {
+    id: 4,
+    name: "YelpCamp",
+    techStack: "MongoDB, Express, EJS and NodeJS",
+    description:
+      "YelpCamp is a simple review site for campgrounds. It is built upon the MVC architecture and uses RESTful API to manage data. The site features user authorization and authentication, campground, reviews and comments CRUD functionalities.",
+    url: "https://secure-castle-27004.herokuapp.com/",
+    githubUrl: "https://github.com/rgel25/yelpcamp",
   },
 ];
 
